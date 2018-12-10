ca3b58c9596a50f36e454f7d9502796915c2c51c
Pablo <pablo.gomez@uk-erlangen.de>
2018-12-10 18:15:08 +0100

    - Small fixes and improvements - Added resnet34 model - There is a memory leak somewhere in runTestSet.ipynb ... -.-

--- Utils/center_images.py
+++ Utils/center_images.py
@@ -1,9 +1,9 @@
 import numpy as np
 from Utils.utils import *
 
-def center_images(X):
+def center_images(X, verbose = False):
     for idx,image in enumerate(X):
-        if idx % 25 == 0:
+        if verbose and idx % 25 == 0:
             printProgressBar (idx, X.shape[0], prefix = 'Centering images...', suffix = '(' + str(idx) + '/' + str(X.shape[0]) + ')')
         image = image - np.mean(image)
         image = image / np.std(image)
