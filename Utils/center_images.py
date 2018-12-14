076d1fdc42a9207b96032b223ff19786893aad78
Pablo <pablo.gomez@uk-erlangen.de>
2018-12-14 10:27:08 +0100

    - Cleaning up - Z scoring / center_images now per channel - added LR scheduling (to be tested) - switched default to big images - added todos - added automatic submission to kaggle

--- Utils/center_images.py
+++ Utils/center_images.py
@@ -5,8 +5,9 @@ def center_images(X, verbose = False):
     for idx,image in enumerate(X):
         if verbose and idx % 25 == 0:
             printProgressBar (idx, X.shape[0], prefix = 'Centering images...', suffix = '(' + str(idx) + '/' + str(X.shape[0]) + ')')
-        image = image - np.mean(image)
-        image = image / np.std(image)
+        for j,channel in enumerate(image):
+            image[j] = channel - np.mean(channel)
+            image[j] = channel / np.std(channel)
         
         X[idx] = image
 
